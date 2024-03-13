0e67597f2b08769a51787d87980a0ed6ef5dceba
Seamus McCarthy <seamusmccarthy2508@gmail.com>
2024-03-13 11:33:15 +0000

    Initial commit

---
+++ test/ewd-assignment-1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EwdAssignment1 from '../lib/ewd-assignment-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ewd-assignment-1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EwdAssignment1.EwdAssignment1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
