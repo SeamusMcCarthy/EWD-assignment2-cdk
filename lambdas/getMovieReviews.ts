0b3e6b4509bdb42711a05eee30fd1cd11de9be37
Seamus McCarthy <seamusmccarthy2508@gmail.com>
2024-03-14 21:49:09 +0000

    minRating param working with string and numeric values

--- lambdas/getMovieReviews.ts
+++ lambdas/getMovieReviews.ts
@@ -2,10 +2,16 @@ import { APIGatewayProxyHandlerV2, Handler } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
-  GetCommand,
   QueryCommand,
   QueryCommandInput,
 } from "@aws-sdk/lib-dynamodb";
+import Ajv from "ajv";
+import schema from "../shared/types.schema.json";
+
+const ajv = new Ajv({ coerceTypes: true });
+const isValidQueryParams = ajv.compile(
+  schema.definitions["MovieReviewQueryParameters"] || {}
+);
 
 const ddbDocClient = createDDbDocClient();
 
@@ -26,32 +32,51 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
         body: JSON.stringify({ Message: "Missing movie Id" }),
       };
     }
-    // const commandOutput = await ddbDocClient.send(
-    //   new GetCommand({
-    //     TableName: process.env.TABLE_NAME,
-    //     Key: { movieId: movieId },
-    //   })
-    // );
-    // if (!commandOutput.Item) {
-    //   return {
-    //     statusCode: 404,
-    //     headers: {
-    //       "content-type": "application/json",
-    //     },
-    //     body: JSON.stringify({ Message: "Invalid movie Id" }),
-    //   };
-    // }
-    // const movieId = parseInt(queryParams.movieId);
+
+    const queryParams = event.queryStringParameters;
+    // const rating = queryParams?.minRating
+    //   ? parseInt(queryParams.minRating)
+    //   : undefined;
+
+    if (queryParams) {
+      // if (!isValidQueryParams(queryParams) || rating === undefined) {
+      if (!isValidQueryParams(queryParams)) {
+        return {
+          statusCode: 500,
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            message: `Incorrect type. Must match Query parameters schema`,
+            schema: schema.definitions["MovieReviewQueryParameters"],
+          }),
+        };
+      }
+    }
+
     let commandInput: QueryCommandInput = {
       TableName: process.env.TABLE_NAME,
     };
-    commandInput = {
-      ...commandInput,
-      KeyConditionExpression: "movieId = :m",
-      ExpressionAttributeValues: {
-        ":m": movieId,
-      },
-    };
+
+    if (queryParams) {
+      commandInput = {
+        ...commandInput,
+        IndexName: "ratingIx",
+        KeyConditionExpression: "movieId = :m and rating >= :r ",
+        ExpressionAttributeValues: {
+          ":m": movieId,
+          ":r": queryParams.minRating,
+        },
+      };
+    } else {
+      commandInput = {
+        ...commandInput,
+        KeyConditionExpression: "movieId = :m",
+        ExpressionAttributeValues: {
+          ":m": movieId,
+        },
+      };
+    }
     const commandOutput = await ddbDocClient.send(
       new QueryCommand(commandInput)
     );
@@ -67,6 +92,17 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
     };
   } catch (error: any) {
     console.log(JSON.stringify(error));
+    // if (isNaN(error.rating)) {
+    //   return {
+    //     statusCode: 500,
+    //     headers: {
+    //       "content-type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       message: `Incorrect type. Minimum rating value must be an integer`,
+    //     }),
+    //   };
+    // }
     return {
       statusCode: 500,
       headers: {
